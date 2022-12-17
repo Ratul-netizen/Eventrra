3b3ced76ea986b0158085080edf45abc4e972d6b
Ratul-netizen <md.wahid.sadiq.ratul@g.bracu.ac.bd>
2022-12-17 14:48:02 +0600

    update

--- lib/view/check_out/checkout_screen.dart
+++ lib/view/check_out/checkout_screen.dart
@@ -2,9 +2,7 @@
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:get/get.dart';
-import 'package:google_fonts/google_fonts.dart';
 import '../../services/payment_service/payment_service.dart';
 import '../../utils/app_color.dart';
 import '../../widgets/my_widgets.dart';
